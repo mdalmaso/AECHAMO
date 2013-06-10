f7762146e9c1cc59deeb1acdf45ce3d0feafbf13
Pauli Simonen <pauli.simonen@tut.fi>
2013-06-10 12:03:39 +0300

    Model_convert gives now smoothened distribution for fixed sections

--- @chamber/model_convert.m
+++ @chamber/model_convert.m
@@ -3,9 +3,9 @@ function[out_struct] = model_convert(obj,t,Y)
 if(obj.initials.fixed_sections == 0)
     out_struct = model_convert_moving_sec(obj,t,Y);
 else
-    out_struct.orig = model_convert_moving_sec(obj,t,Y);
-    out_struct.new = model_convert_moving_center(obj,t,Y);
-    
+%     out_struct.orig = model_convert_moving_sec(obj,t,Y);
+%     out_struct.new = model_convert_moving_center(obj,t,Y);
+    out_struct = model_convert_moving_center(obj,t,Y);
 end
 
 end
