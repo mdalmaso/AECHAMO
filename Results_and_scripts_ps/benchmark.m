28d25aa45c3280f7c8fa0f984c9bf97517425671
Pauli Simonen <pauli.simonen@tut.fi>
2013-06-13 08:43:12 +0300

    Benchmark scripts and some results

---
+++ Results_and_scripts_ps/benchmark.m
@@ -0,0 +1,6 @@
+chamber_runfile('Results_and_scripts_ps/bench_60secs.m');
+clear all;
+chamber_runfile('Results_and_scripts_ps/bench_90secs.m');
+clear all;
+chamber_runfile('Results_and_scripts_ps/bench_100secs.m');
+clear all;
