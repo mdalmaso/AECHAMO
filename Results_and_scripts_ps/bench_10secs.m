4c59eaaa981393d3dfcd9a477b2573f6ea8c5804
Pauli Simonen <pauli.simonen@tut.fi>
2013-06-13 17:02:31 +0300

    New benchmark scripts

--- Results_and_scripts_ps/bench_10secs.m
+++ Results_and_scripts_ps/bench_10secs.m
@@ -1,311 +1,160 @@
 % Benchmark using 10 sections
 
-%% mu 1:
-# 1
+%% Part 1: N values are constant
+%% Part 1.1: Cvap0, sigma and mu are constants
+# 1.11
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
 Dp_min = -9;
 Dp_max = -6;
 tvect = 0:60:32400;
-sigma = [1.1, 1.4];
+sigma = [1.25, 1.3];
 
 N = [1e3 1e2];
-mu=[7e-9, 150e-9];
+mu=[50e-9, 140e-9];
 dilu_on = 0;
 sections = 10;
 output_sections = 10*sections;
 Cvap_const = 1;
-Cvap0 = 1.4e7;
+Cvap0 = 2e7;
 
 gas_source = 0;
 
-# 2
+# 1.12
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
 Dp_min = -9;
 Dp_max = -6;
 tvect = 0:60:32400;
-sigma = [1.1, 1.4];
+sigma = [1.25, 1.3];
 
 N = [1e3 1e2];
-mu=[7e-9, 150e-9];
+mu=[50e-9, 140e-9];
 dilu_on = 1;
 sections = 10;
 output_sections = 10*sections;
 Cvap_const = 1;
-Cvap0 = 1.4e7;
+Cvap0 = 2e7;
 
 gas_source = 0;
 
-
-# 3
+%% Part 1.2: change the Cvap0, sigma and mu
+# 1.21
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
 Dp_min = -9;
 Dp_max = -6;
 tvect = 0:60:32400;
-sigma = [1.1, 1.4];
+sigma = [1.25, 1.4];
 
 N = [1e3 1e2];
-mu=[7e-9, 150e-9];
-dilu_on = 1;
-sections = 10;
-output_sections = 10*sections;
-Cvap_const = 1;
-Cvap0 = 5e7;
-
-gas_source = 0;
-
-# 3
-fixed_sections = 1;
-sedi_on = 0;
-coag_on = 1;
-Dp_min = -9;
-Dp_max = -6;
-tvect = 0:60:32400;
-sigma = [1.1, 1.4];
-
-N = [1e3 1e2];
-mu=[7e-9, 150e-9];
+mu=[40e-9, 200e-9];
 dilu_on = 0;
 sections = 10;
 output_sections = 10*sections;
 Cvap_const = 1;
-Cvap0 = 5e7;
+Cvap0 = 5e8;
 
 gas_source = 0;
 
-# 3
+# 1.22
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
 Dp_min = -9;
 Dp_max = -6;
 tvect = 0:60:32400;
-sigma = [1.1, 1.4];
-
-N = [1e4 1e3];
-mu=[7e-9, 150e-9];
-dilu_on = 0;
-sections = 10;
-output_sections = 10*sections;
-Cvap_const = 1;
-Cvap0 = 1.4e7;
-
-gas_source = 0;
-
-# 3
-fixed_sections = 1;
-sedi_on = 0;
-coag_on = 1;
-Dp_min = -9;
-Dp_max = -6;
-tvect = 0:60:32400;
-sigma = [1.1, 1.4];
-
-N = [1e4 1e3];
-mu=[7e-9, 150e-9];
-dilu_on = 1;
-sections = 10;
-output_sections = 10*sections;
-Cvap_const = 1;
-Cvap0 = 1.4e7;
-
-gas_source = 0;
-
-# 3
-fixed_sections = 1;
-sedi_on = 0;
-coag_on = 1;
-Dp_min = -9;
-Dp_max = -6;
-tvect = 0:60:32400;
-sigma = [1.1, 1.4];
-
-N = [1e4 1e3];
-mu=[7e-9, 150e-9];
-dilu_on = 1;
-sections = 10;
-output_sections = 10*sections;
-Cvap_const = 1;
-Cvap0 = 5e7;
-
-gas_source = 0;
-
-# 3
-fixed_sections = 1;
-sedi_on = 0;
-coag_on = 1;
-Dp_min = -9;
-Dp_max = -6;
-tvect = 0:60:32400;
-sigma = [1.1, 1.4];
-
-N = [1e4 1e3];
-mu=[7e-9, 150e-9];
-dilu_on = 0;
-sections = 10;
-output_sections = 10*sections;
-Cvap_const = 1;
-Cvap0 = 5e7;
-
-gas_source = 0;
-
-%% mu 2
-# 1
-fixed_sections = 1;
-sedi_on = 0;
-coag_on = 1;
-Dp_min = -9;
-Dp_max = -6;
-tvect = 0:60:32400;
-sigma = [1.1, 1.4];
+sigma = [1.25, 1.4];
 
 N = [1e3 1e2];
-mu=[30e-9, 80e-9];
-dilu_on = 0;
-sections = 10;
-output_sections = 10*sections;
-Cvap_const = 1;
-Cvap0 = 1.4e7;
-
-gas_source = 0;
-
-# 2
-fixed_sections = 1;
-sedi_on = 0;
-coag_on = 1;
-Dp_min = -9;
-Dp_max = -6;
-tvect = 0:60:32400;
-sigma = [1.1, 1.4];
-
-N = [1e3 1e2];
-mu=[30e-9, 80e-9];
+mu=[40e-9, 200e-9];
 dilu_on = 1;
 sections = 10;
 output_sections = 10*sections;
 Cvap_const = 1;
-Cvap0 = 1.4e7;
+Cvap0 = 5e8;
 
 gas_source = 0;
 
-
-# 3
+%% Part 2: change the N values
+%% Part 2.1: Cvap0, sigma and mu are constants
+# 2.11
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
 Dp_min = -9;
 Dp_max = -6;
 tvect = 0:60:32400;
-sigma = [1.1, 1.4];
+sigma = [1.25, 1.3];
 
-N = [1e3 1e2];
-mu=[30e-9, 80e-9];
-dilu_on = 1;
-sections = 10;
-output_sections = 10*sections;
-Cvap_const = 1;
-Cvap0 = 5e7;
-
-gas_source = 0;
-
-# 3
-fixed_sections = 1;
-sedi_on = 0;
-coag_on = 1;
-Dp_min = -9;
-Dp_max = -6;
-tvect = 0:60:32400;
-sigma = [1.1, 1.4];
-
-N = [1e3 1e2];
-mu=[30e-9, 80e-9];
+N = [5e4 3e3];
+mu=[50e-9, 140e-9];
 dilu_on = 0;
 sections = 10;
 output_sections = 10*sections;
 Cvap_const = 1;
-Cvap0 = 5e7;
+Cvap0 = 2e7;
 
 gas_source = 0;
 
-# 3
+# 2.12
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
 Dp_min = -9;
 Dp_max = -6;
 tvect = 0:60:32400;
-sigma = [1.1, 1.4];
+sigma = [1.25, 1.3];
 
-N = [1e4 1e3];
-mu=[30e-9, 80e-9];
-dilu_on = 0;
-sections = 10;
-output_sections = 10*sections;
-Cvap_const = 1;
-Cvap0 = 1.4e7;
-
-gas_source = 0;
-
-# 3
-fixed_sections = 1;
-sedi_on = 0;
-coag_on = 1;
-Dp_min = -9;
-Dp_max = -6;
-tvect = 0:60:32400;
-sigma = [1.1, 1.4];
-
-N = [1e4 1e3];
-mu=[30e-9, 80e-9];
+N = [5e4 3e3];
+mu=[50e-9, 140e-9];
 dilu_on = 1;
 sections = 10;
 output_sections = 10*sections;
 Cvap_const = 1;
-Cvap0 = 1.4e7;
+Cvap0 = 2e7;
 
 gas_source = 0;
 
-# 3
+%% Part 2.2: change the Cvap0, sigma and mu
+# 2.21
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
 Dp_min = -9;
 Dp_max = -6;
 tvect = 0:60:32400;
-sigma = [1.1, 1.4];
+sigma = [1.25, 1.4];
 
-N = [1e4 1e3];
-mu=[30e-9, 80e-9];
-dilu_on = 1;
-sections = 10;
-output_sections = 10*sections;
-Cvap_const = 1;
-Cvap0 = 5e7;
-
-gas_source = 0;
-
-# 3
-fixed_sections = 1;
-sedi_on = 0;
-coag_on = 1;
-Dp_min = -9;
-Dp_max = -6;
-tvect = 0:60:32400;
-sigma = [1.1, 1.4];
-
-N = [1e4 1e3];
-mu=[30e-9, 80e-9];
+N = [5e4 3e3];
+mu=[40e-9, 200e-9];
 dilu_on = 0;
 sections = 10;
 output_sections = 10*sections;
 Cvap_const = 1;
-Cvap0 = 5e7;
+Cvap0 = 5e8;
 
 gas_source = 0;
 
+# 2.22
+fixed_sections = 1;
+sedi_on = 0;
+coag_on = 1;
+Dp_min = -9;
+Dp_max = -6;
+tvect = 0:60:32400;
+sigma = [1.25, 1.4];
+
+N = [5e4 3e3];
+mu=[40e-9, 200e-9];
+dilu_on = 1;
+sections = 10;
+output_sections = 10*sections;
+Cvap_const = 1;
+Cvap0 = 5e8;
+
+gas_source = 0;
 #
