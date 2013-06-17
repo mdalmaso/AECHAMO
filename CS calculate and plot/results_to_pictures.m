b75c0a39d716047a7169a2d3cd1305373669d141
Mikko Poikkimäki <mikko.poikkimaki@tut.fi>
2013-06-17 18:15:57 +0300

    Validation pictures

--- CS calculate and plot/results_to_pictures.m
+++ CS calculate and plot/results_to_pictures.m
@@ -1,10 +1,33 @@
-ajoja = 3;
+ajoja = 8;
 
 for i = 1:ajoja
-    chamb(i).plot
+    chamb(i).plot('dist')
 end
+
+chamb(1).plot('dist')
 
 CS = zeros(2881,ajoja);
 for i = 1:ajoja
     CS(1:end,i) = CS_tot(chamb(i).output_data.distr);
 end
+
+%Vtot end
+sect = [10, 20, 30, 40, 60, 90];
+for i = 1:ajoja
+   Ntot = chamb(i).output_data.Ntot(end);
+   figure(1);
+   plot(i ,Ntot, 'm*')   
+   hold on;
+   axis([0,9, 0, 6000])
+end
+
+%Vtot end
+sect = [10, 20, 30, 40, 60, 90];
+for i = 1:ajoja   
+   Vtot =chamb(i).output_data.Vtot(end);
+   figure(2);
+   plot(i, Vtot, 'm*')
+   hold on;
+   axis([0,9, 0, 8e-18])
+end
+
