dec46c4269ea75147b45b200f7f9a510a4927f72
Mikko Poikkimäki <mikko.poikkimaki@tut.fi>
2013-06-17 15:45:56 +0300

    added validation script and results

---
+++ Results and scripts_mp/validation 17.6/Validation_17062013.m
@@ -0,0 +1,6 @@
+chamber_runfile('17062013_validation_10_sect.m')
+chamber_runfile('17062013_validation_20_sect.m')
+chamber_runfile('17062013_validation_30_sect.m')
+chamber_runfile('17062013_validation_40_sect.m')
+chamber_runfile('17062013_validation_60_sect.m')
+chamber_runfile('17062013_validation_90_sect.m')
