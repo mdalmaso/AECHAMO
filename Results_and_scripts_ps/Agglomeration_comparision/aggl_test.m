ec08984c51b58dc5a661b088b0c16302c33206a6
Pauli Simonen <pauli.simonen@tut.fi>
2013-06-27 12:24:42 +0300

    Agglomeration scripts

---
+++ Results_and_scripts_ps/Agglomeration_comparision/aggl_test.m
@@ -0,0 +1,27 @@
+# Agglomeration test
+
+tvect=0:10:1000;
+
+fixed_sections = 1;
+sedi_on = 0;
+coag_on = 1;
+dilu_on = 0;
+
+coag_mode = 'aggl';
+
+Dp_min = -10;
+Dp_max = -6;
+sections = 100;
+output_sections = 1000;
+Cvap0 = 0;
+gas_source = 0;
+
+Df=2.0;
+r0 = 2.5e-9;
+particle_dens = 3.4;
+
+mu=[5e-9];
+sigma = [1.668];
+N = 3e6;
+
+#
