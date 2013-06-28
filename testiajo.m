a423eb201d2f8fe474ee9e022a3554330b3b9af8
Pauli Simonen <pauli.simonen@tut.fi>
2013-06-28 15:25:31 +0300

    New agglomeration tests

--- testiajo.m
+++ testiajo.m
@@ -1,20 +1,23 @@
-# % 3,
+# % 1, 30 sections, Nucleation event everyday between 11:00-13:00
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
 dilu_on = 0;
 
-Dp_min = -8;
+Dp_min = -9;
 Dp_max = -6;
 sections = 30;
 
-tvect = 0:600:86400; % time starts and stops at 00:00
+tvect = 0:600:3600*24; % time starts and stops at 00:00
 
-% Q = alfa*k*P, k = k_O_3*[O_3], P
-gas_source = 0.6*2e-16*60e-9*2.6908e19*100e-9*2.6908e19;
+part_source(:,:,1) = [tvect' [zeros(1,66), 1.0.*ones(1,12), zeros(1,67)]' (3e-9.*ones(1,length(tvect)))']; % 11h no event, 2h event, 11h no event
 
-mu=[100e-9];
-sigma = [1.3];
-N = [30000]; 
+Cvap_const = 0;
+Cvap0 = 0;
+gas_source = 5e4;
+
+mu=100e-9;
+sigma = 1.3;
+N = 0;
 
 #
