fffca0189aa8a7aee6e4eaf741d266c7644c92c7
Pauli Simonen <pauli.simonen@tut.fi>
2013-06-28 15:31:56 +0300

    New agglomeration tests

---
+++ aggl_test.m
@@ -0,0 +1,27 @@
+# Agglomeration test
+
+tvect=0:10:1500;
+
+fixed_sections = 1;
+sedi_on = 0;
+coag_on = 1;
+dilu_on = 0;
+
+coag_mode = 'aggl';
+
+Dp_min = -11;
+Dp_max = -6;
+sections = 80;
+output_sections = 800;
+Cvap0 = 0;
+gas_source = 0;
+
+Df=2.0;
+r0 = 2e-9;
+particle_dens = 3.4;
+
+mu=[5e-10];
+sigma = [1.63];
+N = 5e8;
+
+#
