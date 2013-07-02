aa2ee9a07f9ada361d70480b004e32b98cb03fe5
Pauli Simonen <pauli.simonen@tut.fi>
2013-07-02 12:27:40 +0300

    Updated test script

--- testiajo.m
+++ testiajo.m
@@ -1,20 +1,27 @@
-# % 3,
+#
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
-dilu_on = 0;
+dilu_on = 1;
+dilu_coeff = [0];
 
-Dp_min = -8;
-Dp_max = -6;
-sections = 30;
+vap_wallsink_on = 1;
+vap_wallsink  = [1/9];
 
-tvect = 0:600:86400; % time starts and stops at 00:00
+Dp_min = -9;
+Dp_max = -5;
+sections = 40;
 
-% Q = alfa*k*P, k = k_O_3*[O_3], P
-gas_source = 0.6*2e-16*60e-9*2.6908e19*100e-9*2.6908e19;
+tvect = 0:600:10*24*3600; % time starts and stops at 00:00
 
-mu=[100e-9];
-sigma = [1.3];
-N = [30000]; 
+%part_source(:,:,1) = [tvect' [zeros(1,66), 1.0.*ones(1,12), zeros(1,66), zeros(1,66), 1.0.*ones(1,12), zeros(1,66), zeros(1,66), 1.0.*ones(1,12), zeros(1,66), zeros(1,66), 1.0.*ones(1,12), zeros(1,66), zeros(1,66), 1.0.*ones(1,12), zeros(1,66), zeros(1,66), 1.0.*ones(1,12), zeros(1,66), zeros(1,66), 1.0.*ones(1,12), zeros(1,66), zeros(1,66), 1.0.*ones(1,12), zeros(1,66), zeros(1,66), 1.0.*ones(1,12), zeros(1,66),zeros(1,66), 1.0.*ones(1,12), zeros(1,67)]' (3e-9.*ones(1,length(tvect)))']; % 11h no event, 2h event, 11h no event
 
-#
+% gas_source(:,:,1) = [tvect' [zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,66),zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,66),zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,66),zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,66),zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,66),zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,66),zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,66),zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,66),zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,66),zeros(1,66), 0.3*2e-16*60e-9*2.6908e19*100e-9*2.6908e19.*ones(1,12), zeros(1,67)]']; % 11h no event, 2h event, 11h no event
+gas_source (:,:,1) = create_source(tvect,7200,1.8767e12,39600);
+Cvap0 = 0;
+
+mu=[10e-9];
+sigma = 1.3;
+N = [4000];
+
+# 
