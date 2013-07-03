01bf84d35f34d3d08434991b02b351b2575aa360
Pauli Simonen <pauli.simonen@tut.fi>
2013-07-03 15:23:48 +0300

    New agglomeration tests and documentation

--- aggl_test.m
+++ aggl_test.m
@@ -1,15 +1,15 @@
 # Agglomeration test
 
-tvect=0:10:10000;
+tvect=0:10:5000;
 
 fixed_sections = 1;
 sedi_on = 0;
 coag_on = 1;
 dilu_on = 0;
 
-coag_mode = 'aggl';
+coag_mode = 'coag';
 
-Dp_min = -11;
+Dp_min = -9;
 Dp_max = -6;
 sections = 60;
 output_sections = 600;
@@ -20,8 +20,8 @@ Df=2.0;
 r0 = 3e-10;
 particle_dens = 3.4;
 
-mu=[3e-10];
-sigma = [1.67];
-N = 5e8;
+mu=[20e-9];
+sigma = [1.498];
+N = 1e5;
 
 #
