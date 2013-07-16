3263cd618f6f3dbe7d904cb311bc153b2203a399
Pauli Simonen <pauli.simonen@tut.fi>
2013-07-16 13:48:53 +0300

    New results

--- Results_and_scripts_ps/chamberdata/modify_data.m
+++ Results_and_scripts_ps/chamberdata/modify_data.m
@@ -1,8 +1,12 @@
 
-day=19;
+day=29;
 
-time_start = v(2,1)+(day-18)+0.5832;
-time_end = v(2,1)+(day-18+1)+0.5832;
+% time_start = v(2,1)+(day-18)+0.5832;
+time_start = datenum(2009,9,day,0,0,0);
+
+% time_end = v(2,1)+(day-18+1)+0.5832;
+time_end = datenum(2009,9,day+1,0,0,0);
+
 time = v(2:end,1);
 A=find(time <= time_start);
 begin = A(end);
@@ -13,14 +17,14 @@ last = A(1);
 
 % time = v(begin:begin+width2,1);
 time = v(begin:last,1);
-time = (time-v(2,1)-0.5832-(day-18))*24; % Time in hours
+% time = (time-v(2,1)-0.5832-(day-18))*24; % Time in hours
 
 % dist = [v(1,:);v(begin:begin+width2,:)];
 dist = [v(1,:);v(begin:last,:)];
 % dist(2:end,1) = time;
 
 time_a = a(:,1);
-time_a = (time_a-v(2,1)-0.5832-(day-18))*24;
+% time_a = (time_a-v(2,1)-0.5832-(day-18))*24;
 A=find(time_a <= time(1));
 ind_first = A(end);
 A=find(time_a >= time(end));
@@ -35,6 +39,7 @@ data.MT_plant = matrixdata(:,12); % ppb
 data.inflow = matrixdata(:,14).*(1000/60); % cm^3/s
 data.dilu = matrixdata(:,16).*(1000/60); % cm^3/s
 data.MT_rc = matrixdata(:,10); % ppb
+data.isoprene_plant = matrixdata(:,13); % ppb
 
 V_pc=1.46e6; % cm^3
 NA = 6.022e23; % 1/mol
@@ -43,16 +48,25 @@ inflow_mols = data.inflow.*mol_in_cm3; % cm^3/s*mol/cm^3 = mol/s
 N_tot = inflow_mols.*NA; % mol/s*1/mol = 1/s Includes all molecules
 N_mt = N_tot.*data.MT_plant./1e9; % 1/s
 N_mt_rc = data.MT_rc.*mol_in_cm3.*NA./1e9; % 1/cm^3
+N_isoprene = N_tot.*data.isoprene_plant./1e9;
 
-alfa = 0.83;
+alfa = 0.6;
+
+% alfa=0.5;
 cond_vap_rc = alfa.*N_mt_rc; % Mitattu MT-konsentr. kertaa alfa.
+
 Q=alfa.*N_mt./V_pc; % 1/s*1/cm^3 = 1/(cm^3s)
+
+% test:
+% Q=alfa.*(N_mt+N_isoprene)./V_pc;
+% Q=(alfa.*N_mt + beta.*N_isoprene)./V_pc;
+
 dilu = data.dilu./V_pc;
 
 Dps=dist(1,3:end);
-
-dist_fixed=dist;
-dist_fixed(:,1)=dist_fixed(:,1).*3600;
+% 
+% dist_fixed=dist;
+% dist_fixed(:,1)=dist_fixed(:,1).*3600;
 
 for i=begin:last
     Vtot(i-begin+1)=trapz(log10(Dps),v(i,3:end).*(pi/6.*v(1,3:end).^3));
--- Results_and_scripts_ps/chamberdata/runfile2.m
+++ Results_and_scripts_ps/chamberdata/runfile2.m
@@ -23,14 +23,24 @@ kam.initialize('fixed_sections', 1,...
                'mu', 37e-9,...
                'sigma', 1.6);
 
-part_source(:,:,1) = [tvect' [zeros(866,1);5.5.*ones(28,1);zeros(length(tvect)-866-28,1)], 10e-9.*ones(length(tvect),1)];
-part_source(:,:,2) = [tvect' [zeros(869,1);zeros(26,1);1.7.*ones(108,1);2.6.*ones(170,1);zeros(length(tvect)-869-26-108-170,1)], 18e-9.*ones(length(tvect),1)];
+part_source(:,:,1) = [tvect' [zeros(915,1);11.5.*ones(30,1);zeros(length(tvect)-915-30,1)], 10e-9.*ones(length(tvect),1)];
+part_source(:,:,2) = [tvect' [zeros(915,1);zeros(30,1);4.5.*ones(18,1);1.65.*ones(62,1);2.3.*ones(60,1);0.6.*ones(98,1);1.4.*ones(132,1);zeros(length(tvect)-915-30-18-62-60-98-132,1)], 18e-9.*ones(length(tvect),1)];
+
+timevector = datevec(time_a);
+
+A=find(timevector(:,4) == 0);
+begin = A(1);
+clear A;
+
+timehours = timevector(begin:end,4)+timevector(begin:end,5)./60+timevector(begin:end,6)./3600;
+clear timevector;
 
 
+gas_source = [tvect' [interp1(timehours.*3600,Q(begin:end),tvect(1:1314)),zeros(1,length(tvect)-1314)]'];
 
-gas_source = [tvect' [interp1(time_a.*3600,Q,tvect(1:1241)),zeros(1,length(tvect)-1241)]'];
+dilu_coeff = [tvect' [interp1(timehours.*3600,dilu(begin:end),tvect)]'];
+clear begin;
 
-dilu_coeff = [tvect' [interp1(time_a.*3600,dilu,tvect)]'];
 
 gas_source(find(isnan(gas_source)))=nanmean(Q);
 dilu_coeff(find(isnan(dilu_coeff)))=nanmean(dilu);
@@ -49,20 +59,31 @@ delete(gcc);
 % 
 % figure;
 % subplot_dmps(dist_fixed,111);
+
+timevector=datevec(time);
+A=find(timevector(:,4) == 0);
+begin = A(1);
+clear A;
+timevector = timevector(begin:end,:);
+
+timeax = timevector(:,4)+timevector(:,5)./60+timevector(:,6)./3600;
+
+
 figure('Color',[1 1 1]);
-p1=plot(time,Ntot,'r--');
+p1=plot(timeax,Ntot(begin:end),'r--');
 set(p1,'LineWidth',2);
 hold on;
 p1=plot(kam.output_data.tim./3600,kam.output_data.Ntot);
 set(p1,'LineWidth',2);
 xlim([14 24]);
+% ylim([0 5000]);
 xlabel('time (h)');
 ylabel('Ntot (1/cm^3)');
 set(gca,'FontSize',18);
 set(findall(gcf,'type','text'),'FontSize',18);
 
 figure('Color',[1 1 1]);
-p2=plot(time,Vtot,'r--');
+p2=plot(timeax,Vtot(begin:end),'r--');
 set(p2,'LineWidth',2);
 hold on;
 p2=plot(kam.output_data.tim./3600,kam.output_data.Vtot);
@@ -74,7 +95,7 @@ set(gca,'FontSize',18);
 set(findall(gcf,'type','text'),'FontSize',18);
 
 figure('Color',[1 1 1]);
-p3=plot(time,Vtot./Ntot','r--');
+p3=plot(timeax,Vtot(begin:end)./Ntot(begin:end)','r--');
 set(p3,'LineWidth',2);
 hold on;
 p3=plot(kam.output_data.tim./3600,kam.output_data.Vtot./kam.output_data.Ntot);
@@ -86,3 +107,16 @@ ylabel('Vtot / Ntot (m^3)');
 set(gca,'FontSize',18);
 set(findall(gcf,'type','text'),'FontSize',18);
 hold off;
+
+figure('Color',[1 1 1]);
+p4=plot(kam.initials.tvect./3600,kam.initials.part_source(:,2,1)+kam.initials.part_source(:,2,2));
+set(p4,'LineWidth',2);
+xlim([14 24]);
+ylim([0 4.5]);
+xlabel('time (h)');
+ylabel('Nucleation rate (1/cm^3s)');
+set(gca,'FontSize',18);
+set(findall(gcf,'type','text'),'FontSize',18);
+hold off;
+
+clear begin;
