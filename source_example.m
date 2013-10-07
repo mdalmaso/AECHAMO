1a355c9f3cfe3e32f55fb272edda5e24f81a8937
mdalmaso <miikka.dalmaso@tut.fi>
2013-10-07 13:16:23 +0300

    Made a CS_dependent code for EAC presentations

--- source_example.m
+++ source_example.m
@@ -10,19 +10,20 @@ coag_on = 1;
 dilu_on = 1;
 dilu_coeff = 1/(3.*24.*3600);
 
+mu=100e-9;
+sigma = 1.4;
+N = 1000;
+
 Dp_min = -9;
 Dp_max = -6;
 sections = 30;
 
-tvect = 0:600:2*24*3600;
+tvect = 0:600:15*24*3600;
 Cvap0 = 0;
 
-mu=[180e-9];
-sigma = 1.4;
-N = [1000];
+gas_source = create_gas_source(tvect,6*3600,5e4.*(6*3600),10*3600);
 
-gas_source = create_gas_source(tvect,2*3600,7.2e8,9*3600);
-part_source = create_part_source(tvect,5,3.6e4,10*3600,3e-9);
-
+part_source(:,:,1) = create_part_source(tvect,1000/(3.*24.*3600),1000./(3.*3600),0,3e-9);
+part_source(:,:,2) = create_part_source(tvect,1,2*3.6e3,10*3600,3e-9);
 
 # 
